50b68c047f65f78d0864e1ecbacecea5601ed0e1
Xiao Furen <xfr@dr.com>
2023-11-11 20:36:31 +0800

    add lxc comment

--- 2install.sh
+++ 2install.sh
@@ -1,3 +1,6 @@
+# lxc.cgroup2.devices.allow: c 10:200 rwm
+# lxc.mount.entry: /dev/net/tun dev/net/tun none bind,create=file
+
 apt update
 apt install -y curl sudo
 curl -fsSL https://get.docker.com -o install-docker.sh
