4373eee7ccaa063b87318be2692dcaa722ac1ceb
Xiao Furen <xfr@dr.com>
2023-11-14 01:19:03 +0800

    add simple log

--- alpine/root/entry.sh
+++ alpine/root/entry.sh
@@ -5,8 +5,10 @@
 
 while true
 do
+  echo $(date -u) "tinc staring..." >> /tmp/tinc.log
   /usr/sbin/tincd -D -n xfr 
-  /bin/sleep 9
+  echo $(date -u) "tinc stopped..." >> /tmp/tinc.log
+  sleep 1
 done
 
 /usr/bin/tail -f /dev/null
