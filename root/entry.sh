d7f5c9b8e75f8c9c42820839d7eda4114020da2b
Xiao Furen <xfr@dr.com>
2023-11-14 12:57:41 +0800

    correct entry.sh

--- root/entry.sh
+++ root/entry.sh
@@ -5,8 +5,11 @@
 
 while true
 do
+  echo $(date -u) "tinc staring..." >> /tmp/tinc.log
   /usr/sbin/tincd -D -n xfr 
-  sleep 9
+  echo $(date -u) "tinc stopped..." >> /tmp/tinc.log
+  sleep 1
+  
 done
 
 /usr/bin/tail -f /dev/null
